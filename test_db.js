ae90f295fe277e15c4ec09a2bede7e8a7141d4c8
Huyen Dieu <huyendieummo.workn@gmail.com>
2026-04-13 15:21:41 +0700

    chore: remove temporary test script

--- test_db.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config({ path: '.env.local' });
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-supabase.from('orders').select('*').limit(1).then(res => console.log(JSON.stringify(res.data, null, 2)));
